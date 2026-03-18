5b24769c78796bf6acf0c70f3752cd4cfb0aa94c
Niklas Alén <niklas.k.alen@outlook.com>
2026-03-18 09:24:46 +0200

    Update init.js

--- init.js
+++ init.js
@@ -1,1 +1,1 @@
-init("https://example.com/path/pm.wacz", "https://pohjanmaanmystiikka.blog/ ");
+init("https://github.com/Niklas-gh/pohjanmaanmystiikka/blob/main/pm.wacz", "https://pohjanmaanmystiikka.blog/ ");
